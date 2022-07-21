d00e01bf7ded4b2b3482a87b1c4d8bca9959c10c
nikitinprior <78511432+nikitinprior@users.noreply.github.com>
2022-07-21 14:30:03 +0300

    Create mktemp.c

---
+++ mktemp.c
@@ -0,0 +1,31 @@
+/* $Id: mktemp.c,v 1.2 1994/06/24 11:45:32 ceriel Exp $ */
+/* mktemp - make a name for a temporary file; only here for backwards compat */
+/* no _-protected system-calls? */
+
+unsigned int getpid(void);
+int access(char *, int);
+
+char *mktemp(char *template)
+{
+  register int pid, k;
+  register char *p;
+
+  pid = getpid();		/* get process id as semi-unique number */
+  p = template;
+  while (*p) p++;		/* find end of string */
+
+  /* Replace XXXXXX at end of template with pid. */
+  while (*--p == 'X') {
+	*p = '0' + (pid % 10);
+	pid /= 10;
+  }
+  p++;
+  for (k = 'a'; k <= 'z'; k++) {
+	*p = k;
+	if (access(template, 0) < 0) {
+		return template;
+	}
+  }
+  return("/");
+}
+
